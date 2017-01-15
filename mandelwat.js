8d6b0030d713cbe4ebc4fca7a8a2ce7241eca347
Jeff Fowler <jeffowler@gmail.com>
2017-01-14 21:45:40 -0500

    a predicate!

--- mandelwat.js
+++ mandelwat.js
@@ -2,11 +2,16 @@
     var c = document.getElementById("mandelwat");
     var ctx = c.getContext("2d");
 
+    function predicate(x) {
+        return x % 5 == 0;
+    }
+
     var imageData = ctx.createImageData(c.width, c.height);
-    for (x = 0; x < 200 * 200 * 4; x += 4) {
-        imageData.data[x] =     0;
-        imageData.data[x + 1] = 0;
-        imageData.data[x + 2] = 0;
+    for (x = 0; x < c.width * c.height * 4; x += 4) {
+        var set = predicate(x / 4) ? 0 : 255;
+        imageData.data[x] =     set;
+        imageData.data[x + 1] = set;
+        imageData.data[x + 2] = set;
         imageData.data[x + 3] = 255;
     }
     ctx.putImageData(imageData, 0, 0);
