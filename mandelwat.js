c3c2e687ed5f3a4261f54c06b4343266f9ef7b8e
Jeff Fowler <jeffowler@gmail.com>
2017-01-14 19:35:28 -0500

    square

--- mandelwat.js
+++ mandelwat.js
@@ -1,1 +1,18 @@
-console.log("hello mandelwat");
+(function() {
+    function randColor() { return '#'+Math.floor(Math.random()*16777215).toString(16); }
+
+    var c = document.getElementById("mandelwat");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.rect(0, 0, c.width, c.height);
+
+    function newColor() {
+        ctx.fillStyle = randColor();
+        ctx.fill();
+        console.log(ctx.fillStyle);
+        requestAnimationFrame(newColor);
+    }
+
+    newColor();
+
+})()
