c3d47291023db43c0d79bfa22158664f94eb23c6
Jeff Fowler <jeffowler@gmail.com>
2017-01-14 21:07:18 -0500

    putImageData

--- mandelwat.js
+++ mandelwat.js
@@ -2,12 +2,12 @@
     var c = document.getElementById("mandelwat");
     var ctx = c.getContext("2d");
 
-    ctx.moveTo(c.width/2,0);
-    ctx.lineTo(c.width/2,c.height);
-    ctx.stroke();
-
-    ctx.moveTo(0, c.height / 2);
-    ctx.lineTo(c.width,c.height / 2);
-    ctx.stroke();
-
+    var imageData = ctx.createImageData(c.width, c.height);
+    for (x = 0; x < 200 * 200 * 4; x += 4) {
+        imageData.data[x] =     0;
+        imageData.data[x + 1] = 0;
+        imageData.data[x + 2] = 0;
+        imageData.data[x + 3] = 255;
+    }
+    ctx.putImageData(imageData, 0, 0);
 })()
