1d95c744a86b0fb934f8d2da63f99675958086e4
Jeff Fowler <jeffowler@gmail.com>
2017-01-14 22:05:10 -0500

    xy coords

--- mandelwat.js
+++ mandelwat.js
@@ -2,17 +2,31 @@
     var c = document.getElementById("mandelwat");
     var ctx = c.getContext("2d");
 
-    function predicate(x) {
-        return x % 5 == 0;
+    function predicate(coord) {
+       return coord.x % 10 == 0;
+    }
+
+    function indexToXY(index) {
+        index /= 4;
+        return {
+            x: index % c.width,
+            y: Math.floor(index / c.width)
+        }
     }
 
     var imageData = ctx.createImageData(c.width, c.height);
-    for (x = 0; x < c.width * c.height * 4; x += 4) {
-        var set = predicate(x / 4) ? 0 : 255;
+    var acc = [];
+    for (i = 0; i < c.width * c.height * 4; i += 4) {
+
+        var set = predicate(
+            indexToXY(i)
+        ) ? 0 : 255;
+
         imageData.data[x] =     set;
         imageData.data[x + 1] = set;
         imageData.data[x + 2] = set;
         imageData.data[x + 3] = 255;
     }
+    console.log(acc)
     ctx.putImageData(imageData, 0, 0);
 })()
