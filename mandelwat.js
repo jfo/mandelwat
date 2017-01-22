91221666c7c38cad217a8efe4197b0b288065ec2
Jeff Fowler <jeffowler@gmail.com>
2017-01-22 15:53:52 -0500

    aspect ratio seems to be working

--- mandelwat.js
+++ mandelwat.js
@@ -2,6 +2,8 @@
     function Graph(canvasId) {
         var canvas = document.getElementById(canvasId);
         var ctx = canvas.getContext("2d");
+        var aspectRatio = canvas.height / canvas.width
+
         this.r = 1
         this.center = {
             x: 0,
@@ -14,24 +16,20 @@
                 x: index % canvas.width,
                 y: Math.floor(index / canvas.width)
             }
-            coord.x = ((coord.x * this.r / canvas.height) - this.r / 2) + this.center.x;
-            coord.y = (((coord.y * this.r / canvas.width) - this.r / 2) * -1) + this.center.y ;
+
+            coord.x = (((coord.x * this.r / canvas.width) - this.r / 2) + (this.center.x * aspectRatio)) / aspectRatio;
+            coord.y = ((((coord.y * this.r / canvas.height) - this.r / 2) * -1) + this.center.y);
+
             return coord;
         }.bind(this)
 
         var imageData = ctx.createImageData(canvas.width, canvas.height);
         this.render = function(predicate) {
             for (var i = 0; i < canvas.width * canvas.height * 4; i += 4) {
-                var thing = predicate(indexToCoord(i));
-                set = thing[0] ? 0 : (thing[1] / 200) * 0xffffff;
-
-                if (!thing[0] && set == 0) {
-                    set = 0xffffff;
-                }
-
-                imageData.data[i]     = (set & 0xff0000) >> 16;
-                imageData.data[i + 1] = (set & 0x00ff00) >> 8;
-                imageData.data[i + 2] = set & 0x0000ff;
+                set = predicate(indexToCoord(i)) ? 0 : 255;
+                imageData.data[i]     = set;
+                imageData.data[i + 1] = set;
+                imageData.data[i + 2] = set;
                 imageData.data[i + 3] = 255;
             }
             ctx.putImageData(imageData, 0, 0);
@@ -47,7 +45,7 @@
         var i;
         for (i = 0; i < 200; i++) {
             if (zr**2 + zi**2 > 4) {
-                return [false, i];
+                return false;
             }
 
             newzr = (zr * zr) - (zi * zi) + cr;
@@ -55,11 +53,11 @@
             zr = newzr
             zi = newzi
         }
-        return [true, i];
+        return true;
     }
 
     var graph = new Graph("mandelwat");
-    graph.r = 2
+    graph.r = 0.0003;
     graph.center = {
         x: -1.25066,
         y: 0.02012
@@ -68,7 +66,7 @@
     // setInterval(function(){
     //     graph.r /=1.1;
     //     graph.render(ismandlebrot);
-    // },300);
+    // },200);
 })();
 
 
