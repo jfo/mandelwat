0ec6f766eff951589e4858a03dabb7b42126be0c
Jeff Fowler <jeffowler@gmail.com>
2018-05-19 17:17:22 +0200

    rm unneccesarry function wrappers

--- mandelgl.js
+++ mandelgl.js
@@ -1,49 +1,38 @@
 (function() {
   var canvas = document.getElementById('mandelwat');
-
   var gl = canvas.getContext('webgl');
   if (!gl) {
     alert("This browser doesn't seem to support webGL. sorry :(");
     return;
   }
-
-  function createShader(gl, type, source) {
-    var shader = gl.createShader(type);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-    if (success) {
-      return shader;
-    }
-
+  var vertexShaderSource = document.getElementById('vertex-shader').text;
+  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShader, vertexShaderSource);
+  gl.compileShader(vertexShader);
+  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+    console.error('vertex shader failed to compile');
     console.log(gl.getShaderInfoLog(shader));
     gl.deleteShader(shader);
   }
-
-  var vertexShaderSource = document.getElementById('vertex-shader').text;
   var fragmentShaderSource = document.getElementById('fragment-shader').text;
-  var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-  var fragmentShader = createShader(
-    gl,
-    gl.FRAGMENT_SHADER,
-    fragmentShaderSource,
-  );
-
-  function createProgram(gl, vertexShader, fragmentShader) {
-    var program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
-    if (success) {
-      return program;
-    }
-
+  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShader, fragmentShaderSource);
+  gl.compileShader(fragmentShader);
+  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+    console.error('vertex shader failed to compile');
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+  }
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.error('program failed to link');
     console.log(gl.getProgramInfoLog(program));
     gl.deleteProgram(program);
   }
 
-  var program = createProgram(gl, vertexShader, fragmentShader);
   var positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
   var positionBuffer = gl.createBuffer();
 
